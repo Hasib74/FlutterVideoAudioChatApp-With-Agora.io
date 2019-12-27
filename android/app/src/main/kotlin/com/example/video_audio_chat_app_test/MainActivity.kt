7e29e06e0255e4783b75f35c183bd67606edb1db
hasib74 <hasibakon74@gmail.com>
2019-12-28 02:18:23 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/video_audio_chat_app_test/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.video_audio_chat_app_test
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,2 @@
+// Agora AppId
+const APP_ID = "517bd9e1b280446cbe9d323f2e5164e2";
